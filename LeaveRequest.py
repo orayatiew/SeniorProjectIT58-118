51ab6017d1723cb204cb1b2c72e932ccd8986ebe
Oraya Saetiew <33575263+orayatiew@users.noreply.github.com>
2018-09-15 18:51:51 +0700

    Update LeaveRequest.py

--- LeaveRequest.py
+++ LeaveRequest.py
@@ -1,4 +1,4 @@
-﻿from flask import Flask, request, make_response, jsonify,abort
+from flask import Flask, request, make_response, jsonify,abort
 import json
 import os
 
@@ -14,13 +14,43 @@ log = app.logger
 def leaveRequest(req):
     userId = getUserID(req)
     leavetype = getParamQueryResult(req,'leavetype')
-    ID = getDataMatchUsers(userId,'id')
-    role = getDataMatchUsers(userId,'role')
-    name =getDataFollowRole(str(role),str(ID),'name')
+    ID = getIDFromMatchUser(str(userId))
+    role = getRoleFromMatchUser(str(userId))
+    name = getName(str(role),str(ID))
+    print(ID,role,name)
     if leavetype == 'Business':
-        return 'คุณ '+name +' ต้องการลากิจวิชาอะไรคะ'
+        return 'คุณ '+str(name) +' ต้องการลากิจวิชาอะไรคะ'
     else:
-        return 'คุณ '+name +' ต้องการลาป่วยวิชาอะไรคะ'
+        return 'คุณ '+str(name) +' ต้องการลาป่วยวิชาอะไรคะ'
 
-def leavereqGetpar(req):
-    
+def pushReqToLf(req):
+    userId = getUserID(req)
+    leavetype = getParamOutputcontext(req,'leavetype',1)
+    subject = getParamOutputcontext(req,'subjects',0)
+    section = getParamOutputcontext(req,'section',0)
+    date = getParamOutputcontext(req,'date',0)
+    date = str(date).replace("T12:00:00+00:00","")
+    print(subject,section,date,userId)
+    pushMgsReqToLF(subject,date,userId,section,leavetype)
+    return 'คำขอของนักศึกษาได้ถูกส่งไปยัง LF ประจำวิชาแล้ว เมื่อLF รับทราบแล้วระบบจะส่งแจ้งเตือนมายังนักศึกษาอีกครั้งค่ะ'
+
+def approveReq(req):
+    studentId = getParamQueryResult(req,'ID')
+    role = getParamQueryResult(req,'role')
+    userId = getUserId(role,studentId)
+    print (userId,studentId)
+    message = 'คำขอของคุณได้รับการอนุมัติแล้วค่ะ'
+    pushMessage(str(userId),message)
+    return 'แจ้งการอนุมัติไปยังนักศึกษาเรียบร้อยแล้วค่ะ'
+
+def rejectReq(req):
+    cuase = getQueryRult(req)
+    studentId = getParamOutputcontext(req,"ID",0)
+    role = getParamOutputcontext(req,"role",0)
+    userId = getUserId(role,studentId)
+    print (userId,studentId)
+    message1 = 'คำขอของคุณไม่ได้รับการอนุมัติค่ะ'
+    pushMessage(str(userId),message1)
+    message2 = 'เพราะ '+str(cuase)
+    pushMessage(str(userId),message2)
+    return 'แจ้งการอนุมัติไปยังนักศึกษาเรียบร้อยแล้วค่ะ'  
